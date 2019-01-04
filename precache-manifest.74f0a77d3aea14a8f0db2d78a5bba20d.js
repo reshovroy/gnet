306df46300b83c711a064fe5a06afbc4c6f2c515
reshovroy <reshovr58@gmail.com>
2019-01-05 03:53:50 +0530

    Updates

---
+++ precache-manifest.74f0a77d3aea14a8f0db2d78a5bba20d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceec8dba1b07285e3332",
+    "url": "/gnet/static/js/runtime~main.ceec8dba.js"
+  },
+  {
+    "revision": "5f76b566f25a780032e8",
+    "url": "/gnet/static/js/main.5f76b566.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/gnet/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "5f76b566f25a780032e8",
+    "url": "/gnet/static/css/main.87ea21a6.chunk.css"
+  },
+  {
+    "revision": "89259e379fdef8d2bf69d2db6692ef05",
+    "url": "/gnet/index.html"
+  }
+];
